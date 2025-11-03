bf29843d69552f6012437a8d2249b88d1d676d76
Cristian Figueroa <cristian@zipline.ai>
2025-11-03 10:41:13 -0800

    Move canary and tests

--- python/zipline-hub/test/conftest.py
+++ python/zipline-hub/test/conftest.py
@@ -24,25 +24,10 @@ def rootdir():
 
 
 @pytest.fixture
-def teams_json(rootdir):
-    return os.path.join(rootdir, 'sample/teams.json')
-
-
-@pytest.fixture
-def repo(rootdir):
-    return os.path.join(rootdir, 'sample/')
-
-@pytest.fixture
-def test_online_group_by(repo):
-    return os.path.join(repo, 'production/group_bys/sample_team/event_sample_group_by.v1')
+def canary(rootdir):
+    return os.path.join(rootdir, 'canary/')
 
 @pytest.fixture
 def online_join_conf():
     """ Standard online join for tests """
     return "compiled/joins/gcp/demo.v1__1"
-
-@pytest.fixture
-def sleepless():
-    def justpass(seconds):
-        pass
-    return justpass
