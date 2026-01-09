50b61bf5ffeaefc81dbeb927f02745d9fc0e6983
David Han <david@zipline.ai>
2026-01-09 17:09:00 -0500

    use preagg schema

--- spark/src/main/scala/ai/chronon/spark/GroupByUpload.scala
+++ spark/src/main/scala/ai/chronon/spark/GroupByUpload.scala
@@ -107,8 +107,8 @@ class GroupByUpload(endPartition: String, groupBy: GroupBy) extends Serializable
         .treeAggregate(mutable.HashMap.empty[String, Long])(
           seqOp = { case (counterMap, (_, values)) =>
             if (values != null) {
-              // TODO: we need to handle NPE when groupby has no aggregations
-              groupBy.postAggSchema.foreach { field =>
+              // Using preAggSchema since no aggregations in this if block
+              groupBy.preAggSchema.foreach { field =>
                 val key = field.name
                 counterMap.update(key, counterMap.getOrElse(key, 0L) + 1L)
               }
