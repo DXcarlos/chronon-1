1bb6827aeb0bb0e6dd04d8e640d14b77ed3abf6e
David Han <david@zipline.ai>
2026-01-13 18:38:57 -0500

    update todo

--- api/src/main/scala/ai/chronon/api/Extensions.scala
+++ api/src/main/scala/ai/chronon/api/Extensions.scala
@@ -331,8 +331,8 @@ object Extensions {
       aggregations.foreach { agg =>
         val buckets = Option(agg.buckets)
           .map(_.toScala)
-          // TODO: why doesn't this just return empty seq?
-          .getOrElse(Seq(null))
+          .getOrElse(Seq(null)) // this doesn't return empty Seq because this will act as a "global"
+        // unwindowed AggregationPart. in particular for collpasedIr in temporal cases
         val windows = Option(agg.windows)
           .map(_.toScala)
           .getOrElse(Seq(WindowUtils.Unbounded))
