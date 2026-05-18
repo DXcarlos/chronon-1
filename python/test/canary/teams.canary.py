6ef328717ff9a9d7fd12ba43e6d292472587e1c2
David Han <david@zipline.ai>
2026-05-18 17:40:26 -0400

    show only affects the files in compiled_canary/ to verify that the canary compile uses this teams.canary.py file and not teams.py

--- python/test/canary/teams.canary.py
+++ python/test/canary/teams.canary.py
@@ -188,3 +188,7 @@ azure = Team(
 
 # Show that teams can be imported from other teams.py files
 from teams import aws_databricks
+
+# only affects the files in compiled_canary/ to verify that the canary compile uses this teams.canary.py file and not teams.py
+aws_databricks_env: EnvironmentVariables = aws_databricks.env
+aws_databricks_env.common['DATABRICKS_EXTRA'] = "DATABRICKS_EXTRA_1"
